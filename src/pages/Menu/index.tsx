e2e11ff26932c45ef6b3149369553d345c9bf456
ThiagoPetry <thiagokeiserpetry@gmail.com>
2021-07-19 08:46:16 -0300

    Atualização dos ícones

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -47,13 +47,13 @@ const Menu: React.FC = () => {
                 document.getElementById("user-img")!.style.width = '8vh';
                 document.getElementById("user-img")!.style.height = '9.25vh';
 
-                document.getElementById("news-img")!.style.transform = 'translate(-12vw, 1.8vh)';
+                document.getElementById("news-img")!.style.transform = 'translate(-12vw, 1.6vh)';
                 document.getElementById("news-img")!.style.width = '2.77vh';
                 document.getElementById("news-img")!.style.height = '2.73vh';
                 document.getElementById("news-img")!.style.marginLeft = '12.5vw';
                 document.getElementById("news-img")!.style.justifyContent = 'start';
 
-                document.getElementById("title-not")!.style.marginTop = '1.5vh';
+                document.getElementById("title-not")!.style.marginTop = '1.6vh';
                 document.getElementById("user-name")!.style.margin = '1vh 0 0 16vw';
 
                 document.getElementById("user-name")!.style.display = "block";
@@ -74,7 +74,7 @@ const Menu: React.FC = () => {
                 document.getElementById("news-img")!.style.transform = 'translate(0vw, 0vh)';
                 document.getElementById("news-img")!.style.width = '10vh';
                 document.getElementById("news-img")!.style.height = '6vh';
-                document.getElementById("news-img")!.style.marginLeft = '14.8vw';
+                document.getElementById("news-img")!.style.marginLeft = '12.8vw';
 
                 document.getElementById("user-name")!.style.display = "none";
                 document.getElementById("title-not")!.style.display = "none";
@@ -116,19 +116,19 @@ const Menu: React.FC = () => {
             <ContainerMenu id="container-menu">
                 <ContainerInfo>
                     <UserImg id="user-img">
-                        <FaUserAlt size={40} color="#00579D" />
+                        <FaUserAlt id="iconUser" color="#00579D" />
                         <p id="user-name">Seja bem vindo, <a href="./">Renato Silva</a></p> 
                     </UserImg>
                     <NewsImg id="news-img">
-                        <IoNewspaperOutline size={40} id="icon-news"/>
+                        <IoNewspaperOutline id="iconNews" />
                         <p id="title-not">Notificações</p> 
                     </NewsImg>
                     <LineBlue id="line-blue" />
                     <RefreshImg id="refresh-img">
-                        <FiRefreshCcw size={20} id="icon-ref"/>
+                        <FiRefreshCcw id="iconRef"/>
                     </RefreshImg>
                     <DeleteImg id="delete-img">
-                        <RiDeleteBinLine size={20} id="icon-del"/>
+                        <RiDeleteBinLine id="iconDel"/>
                     </DeleteImg>
                     <ContainerMsg id="container-msg">
                         <Msg>
@@ -145,7 +145,7 @@ const Menu: React.FC = () => {
                         </Msg>
                     </ContainerMsg>
                     <ExitImg id="exit-img">
-                        <IoExitOutline size={40} id="icon-exit"/>
+                        <IoExitOutline id="iconExit"/>
                     </ExitImg>
                 </ContainerInfo>
                 <BtnOpen id="btn-open" onClick={handdleClick} />

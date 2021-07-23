108a8c8a23a86ee0a605af46ccecb4142c693ddd
ThiagoPetry <thiagokeiserpetry@gmail.com>
2021-07-23 16:09:29 -0300

    Estilização dos filtros + cards

--- src/pages/Projects/styles.ts
+++ src/pages/Projects/styles.ts
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 
-
 export const Container = styled.div`
     position: absolute;
     top: 14vh;
@@ -26,6 +25,8 @@ export const ContainerInfo = styled.div`
     width: 100%;
     height: 12vh;
 
+    padding-top: 1vh;
+
     display: flex;
     flex-direction: column;
 `;
@@ -35,6 +36,25 @@ export const ContainerTitle = styled.div`
     height: 12vh;
 
     display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    h1 {
+        color: #00579D;
+        font-size: 3vh;
+        margin-left: 1.2vw;
+
+        display: flex;
+        align-items: center;
+
+        svg {
+            
+        }
+    }
+
+    p {
+        margin-right: 1.2vw;
+    }
 `;
 
 export const ContainerFiltro = styled.div`
@@ -44,32 +64,82 @@ export const ContainerFiltro = styled.div`
     border-bottom: 0.2vh solid #ccc;
 
     display: flex;
+    place-content: flex-start space-around;
+    align-items: center;
+
+    h1 {
+        color: #00579D;
+        font-size: 3vh;
+        margin-left: 1.2vw;
+    }
+
+    div {
+
+        display: flex;
+        align-items: center;
+        
+        label {
+            font-size: 3vh;
+            color: #00579D;
+
+            margin-right: 0.5vw;
+        }
+
+        select {
+            width: 12vw;
+            padding: 0.5vh;
+
+            border-color: #ccc;
+
+            color: #00579D;
+        }
+
+        input {
+            width: 12vw;
+            padding: 0.5vh;
+            padding-left: 1vh;
+
+            border: 0.2vh solid #ccc;
+
+            color: #00579D;
+
+            ::placeholder {
+                font-size: 1.8vh;
+                color: rgb(0, 0, 0, 0.4);
+            }
+        }
+    }
 `;
 
 export const Projects = styled.div`
     width: 100%;
     height: 80vh;
 
-    display: flex;
-    overflow: scroll;
-`;
-
-export const Left = styled.div`
-    width: 50%;
+    align-items: center;    
     
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+    overflow: scroll;
+    overflow-x: hidden;
 
-    overflow: hidden;
+    ::-webkit-scrollbar {
+        background-color: rgb(196, 196, 196, 0.5);
+        width: 0.5vw;
+    }
+       
+    ::-webkit-scrollbar-thumb {
+        background-color: rgb(196, 196, 196); 
+        border-radius: 1vh;
+    }
 `;
 
-export const Right = styled.div`
-    width: 50%;
+export const Center = styled.div`
+    width: 100%;
+
+    &:last-child {
+        margin-bottom: 4vh;
+    }
 
     display: flex;
-    flex-direction: column;
+    place-content: flex-start space-around;
+    flex-direction: row;
     align-items: center;
-
-    overflow: hidden;
 `;

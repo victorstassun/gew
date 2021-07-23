053186f7b8fa6188abd906a491dcbeab7250b5ef
ThiagoPetry <thiagokeiserpetry@gmail.com>
2021-07-23 11:26:22 -0300

    Adição do componente CardProject

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -3,20 +3,56 @@ import React from 'react';
 import MenuLeft from '../components/MenuLeft';
 import Navbar from '../components/Navbar';
 import MenuRight from '../components/MenuRight';
+import CardProject from '../components/CardProject';
+
+import { ContainerProject, ContainerInfo, Projects, Container, ContainerTitle, ContainerFiltro,
+    Left, Right } from './styles';
 
 import { ContIcons } from '../components/MenuRight/styles';
 
-const RegisterConsultants: React.FC = () => {
 
+const RegisterConsultants: React.FC = () => {
     return (
         <>
             <Navbar />
             <MenuLeft />
+
+            <Container>
+                <ContainerProject>
+                    <ContainerInfo>
+                        <ContainerTitle></ContainerTitle>
+                        <ContainerFiltro></ContainerFiltro>
+                    </ContainerInfo>
+                    <Projects>
+                        <Left>
+                            <CardProject /> 
+                            <CardProject />
+                            <CardProject />
+                            <CardProject />
+                            <CardProject /> 
+                            <CardProject />
+                            <CardProject />
+                            <CardProject />
+                        </Left>
+                        <Right>
+                            <CardProject /> 
+                            <CardProject />
+                            <CardProject />
+                            <CardProject />
+                            <CardProject /> 
+                            <CardProject />
+                            <CardProject />
+                            <CardProject />
+                        </Right>
+                    </Projects>
+                </ContainerProject>
+            </Container>
+
             <MenuRight>
-                <ContIcons type={'Projects'} />
+                <ContIcons />
             </MenuRight>
         </>
-        );
+    );
 };
 
 export default RegisterConsultants;

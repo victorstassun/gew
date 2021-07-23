feee877b729eb4a250b0cd7c71e72758d2684afe
ThiagoPetry <thiagokeiserpetry@gmail.com>
2021-07-23 11:26:38 -0300

    Estilização da página de projetos

--- src/pages/Projects/styles.ts
+++ src/pages/Projects/styles.ts
@@ -1,84 +1,75 @@
 import styled from "styled-components";
 
-export const ContainerMenuRight = styled.div`
-    width: 30vw;
-    height: 76vh;
-    margin: 9.5vh 0 0 98vw;
+
+export const Container = styled.div`
+    position: absolute;
+    top: 14vh;
+    left: 12vw;
+    width: 78vw;
+    height: 80vh;
+`;
+
+export const ContainerProject = styled.div`
+    width: 78vw;
+    height: 80vh;
+
     display: flex;
     flex-direction: column;
-    position: fixed;
-`;
-
-export const ContIcons = styled.div`
-    width: 100%;
-    height: 12.3vh;
-    margin-bottom: 0.3vh;
-    background-color: #0091BD;
-    display: flex;
-    justify-content: left;
-    align-items: center;
-    cursor: pointer;
-    transition: all 0.4s;
-
-    &:first-child {
-        margin-left: -0.7vw;
-
-        #icons {
-            margin-left: 1.1vw;
-        }
-        
-        &:before {
-            content: "";
-            width: 0.1vw;
-            height: 10vh;
-            margin-left: 0.8vw;
-            background: #fff;
-            position: absolute;
-        }
-    }
-
-    &:nth-child(1), &:nth-child(2) { background-color: #00579D; }
-    &:nth-child(3), &:nth-child(4) { background-color: #0075B1; }
-    &:nth-child(1) { border-radius: 0.5vh 0 0 0; }
-    &:nth-child(6) { border-radius: 0 0 0 0.5vh; }
-
-    &:hover {
-        transform: translateX(-30vh);
-        opacity: 0.9;
-
-        &:before {
-            display: none;
-        }
-
-        #icons {
-            transform: scale( 3.2, 3.2);
-            margin-left: 5vh;
-        }
-    }
-
-`;
-
-export const Icon = styled.div`
-    #icons {
-        width: 2.75vh;
-        heigth: 3.4vh;
-        margin-left: 0.8vh;
-    }
-`;
-
-export const TextMenuRight = styled.div`
-    width: 8vw;
-    margin-left: 1.5vw;
-    display: flex;
     justify-content: center;
+
+    background-color: #fff;
+    box-shadow: 0.25vh 0.25vh 0.5vh rgb(0, 0, 0, 0.25);
+    border-radius: 0.4vh;
+`;
+
+export const ContainerInfo = styled.div`
+    width: 100%;
+    height: 12vh;
+
+    display: flex;
+    flex-direction: column;
+`;
+
+export const ContainerTitle = styled.div`
+    width: 100%;
+    height: 12vh;
+
+    display: flex;
+`;
+
+export const ContainerFiltro = styled.div`
+    width: 100%;
+    height: 12vh;
+
+    border-bottom: 0.2vh solid #ccc;
+
+    display: flex;
+`;
+
+export const Projects = styled.div`
+    width: 100%;
+    height: 80vh;
+
+    display: flex;
+    overflow: scroll;
+`;
+
+export const Left = styled.div`
+    width: 50%;
+    
+    display: flex;
     flex-direction: column;
     align-items: center;
 
-    a {
-        color: #fff;
-        font-size: 2.5vh;
-        font-weight: 600;
-        margin-left: 3vw;
-        font-family: Arial, Helvetica, sans-serif;
-    }
+    overflow: hidden;
+`;
+
+export const Right = styled.div`
+    width: 50%;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    overflow: hidden;
 `;

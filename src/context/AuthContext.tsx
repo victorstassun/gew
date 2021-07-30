b6f5936c904938b0fc3337d2b291d1f601da9f20
Thiago Keiser Petry <thiagokeiserpetry@gmail.com>
2021-07-30 11:30:32 -0300

    Salva token no localstorage

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,52 @@
+import React, { createContext, useCallback, useState } from "react";
+
+import api from '../service/api';
+
+interface AuthState {
+    jwt: string;
+}
+
+interface SingInCredentials {
+    email: string;
+    senha: string;
+}
+
+interface AuthContextData {
+    singIn(credentials: SingInCredentials): Promise<void>;
+}
+
+export const AuthContext = createContext<AuthContextData>(
+    {} as AuthContextData
+);
+
+export const AuthProvider: React.FC = ({ children }) => {
+    const [data, setData] = useState<AuthState>(() => {
+        const jwt = localStorage.getItem("gew:token");
+
+        if(jwt) {
+            return {jwt};
+        }
+
+        return {} as AuthState;
+    });
+
+    const singIn = useCallback(async ({ email, senha }) => {
+        const response = await api.post("authenticate", {
+            email,
+            senha,
+        });
+
+        console.log(response.data);
+        const { jwt } = response.data;
+
+        localStorage.setItem("gew:token", jwt);
+        setData(jwt);
+        
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{singIn}}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
